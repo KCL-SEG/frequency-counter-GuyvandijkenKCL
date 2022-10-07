75fed0d830929c764bdb81019e68cf7022dc5981
GuyvandijkenKCL <115276064+GuyvandijkenKCL@users.noreply.github.com>
2022-10-07 17:29:10 +0100

    Update frequencies.py
    The solution.

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    
+    for x in range(len(items)):
+        if items[x] in list(frequencies):
+            frequencies[items[x]] = frequencies[items[x]] + 1
+        else:
+            frequencies[items[x]] = 1
+    
     return frequencies
